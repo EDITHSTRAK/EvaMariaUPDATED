67571d2939216dd5f5af04e314565eed184e3344
EDITHSTRAK <111075167+EDITHSTRAK@users.noreply.github.com>
2022-08-13 19:49:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -86,14 +86,13 @@ async def start(client, message):
             )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+        buttons = [ [
+            
+            InlineKeyboardButton('‼️ JOIN OUR COMMUNITY ‼️', url='https://t.me/filmcognito')
+        ], [
+            InlineKeyboardButton('📺 JOIN FOR MOVIES 📺', url='https://t.me/filmcognitomoviesin')  
+        ], [
+            InlineKeyboardButton('🕹️ JOIN FOR LIVE SERIES COLLECTION 🕹️ ',url='https://t.me/filmcognitoseries')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
